a703724f67503a10c73c29c65a3f2c5db72aa84e
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2023-08-22 18:01:57 -0300

    Add files via upload
    Perfect

---
+++ main.js
@@ -0,0 +1,3 @@
+function exit() {
+    window.location.href = "https://www.amazon.com.br/s?k=pokemon+cartas+raras&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=23VMOVLN1JXH3&sprefix=pokemon+cartas+raras%2Caps%2C209&ref=nb_sb_noss_1"
+}
